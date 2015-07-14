3fedb891fafce82c9400d3475a3521a65bdefff3
Lutts Cao <lutts.cao@gmail.com>
2015-07-14 22:13:47 +0800

    add visitor model abstract classes

--- include/pfmvp/i_pf_model.h
+++ include/pfmvp/i_pf_model.h
@@ -23,14 +23,51 @@ class IPfModel {
 };
 
 #define DEF_MODEL_ID(id)                                        \
-  const ModelIdType& getModelId() const override {        \
+  const ModelIdType& getModelId() const override {              \
     return modelId();                                           \
   }                                                             \
                                                                 \
   static const ModelIdType& modelId() {                         \
-    static const ModelIdType model_id { #id };                        \
+    static const ModelIdType model_id { #id };                  \
     return model_id;                                            \
   }
+
+/*!
+ * there are two reasons to have visitable PfModel,
+ *
+ *  1. simplify traverse a list of IPfModel derived models
+ *  2. some times we drived from IPfModel just because we want many
+ *     objects to return in a single list, to simplify the model interface,
+ *     but we don't want to have a complete triad for this type of IPfModel,
+ *     so we need a method to filter these fake models out of the large list,
+ *     visitor pattern is good solution.
+ *     e.g. TextModel, displayed as a QLabel, we don't bother to have a triad
+ *     for this.
+ */
+
+template <typename Visitor>
+class VisitableModel : public IPfModel {
+ public:
+  virtual void accept(Visitor& visitor) = 0;
+};
+
+class IPfModelVisitor {
+ public:
+  virtual ~IPfModelVisitor() = default;
+};
+
+class AcyclicVisitableModel : public IPfModel {
+ public:
+  virtual void accept(IPfModelVisitor& visitor) = 0;
+  /*
+   * typical implementation:
+   *
+   *  try {
+   *    auto& real_visitor = dynamic_cast<XXXVisitor>(visitor);
+   *    real_visitor.visit(this);
+   *  } catch (std::bad_cast&) { }
+   */
+};
 
 }  // namespace pfmvp
 
