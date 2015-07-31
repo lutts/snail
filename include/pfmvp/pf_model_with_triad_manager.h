a1b35b52c4d2d7407a3eed0f089c6abd303d27d6
Lutts Cao <lutts.cao@gmail.com>
2015-07-31 23:49:28 +0800

    add PfModel base class with triad manager helpers

---
+++ include/pfmvp/pf_model_with_triad_manager.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2015
+// All rights reserved.
+//
+// Author: Lutts Cao <<lutts.cao@gmail.com>>
+//
+// [Desc]
+
+#ifndef PF_MODEL_WITH_TRIAD_MANAGER_H_
+#define PF_MODEL_WITH_TRIAD_MANAGER_H_
+
+#include "utils/basic_utils.h"
+#include "pfmvp/i_pf_model.h"
+#include "pfmvp/i_pf_triad_manager.h"
+
+namespace pfmvp {
+
+class PfModelWithTriadManager : public IPfModel {
+ public:
+  PfModelWithTriadManager() = default;
+  virtual ~PfModelWithTriadManager() = default;
+
+  void set_triad_manager(IPfTriadManager* triad_manager) {
+    triad_manager_ = triad_manager;
+  }
+
+  void removeTriadBy(IPfModel* model) {
+    if (!triad_manager_)
+      return;
+
+    triad_manager_->removeTriadBy(model);
+  }
+
+ private:
+  IPfTriadManager* triad_manager_ { nullptr };
+
+ private:
+  SNAIL_DISABLE_COPY(PfModelWithTriadManager)
+};
+
+}  // namespace pfmvp
+
+#endif  // PF_MODEL_WITH_TRIAD_MANAGER_H_
