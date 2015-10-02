86c154300fc22a36db7d7f5a01d00a7d58a4893a
Lutts Cao <lutts.cao@gmail.com>
2015-10-02 11:21:53 +0800

    add qsint actionpanel

--- src/qtui/work_view.cc
+++ src/qtui/work_view.cc
@@ -10,7 +10,13 @@
 #include <QVBoxLayout>
 #include <QHeaderView>
 
+#include <QPushButton>
+#include <QLabel>
+#include <QDebug>
+
 #include "qtui/i_work_basic_info_qmodel.h"
+
+#include "qsint/include/QSint"
 
 WorkView::WorkView() {
   QVBoxLayout *verticalLayout = new QVBoxLayout(this);
@@ -28,11 +34,45 @@ WorkView::WorkView() {
 
   verticalLayout->addWidget(basicInfoView);
 
+  QSint::ActionPanel* action_panel = new QSint::ActionPanel(this);
+
+  QSint::ActionGroup *group1 = action_panel->createGroup(QPixmap(QStringLiteral(":images/filesave.png")), QStringLiteral("Expandable Group"));
+  group1->setHasEditButton(true);
+  group1->setHeaderTextEditable(true);
+
+  group1->addWidget(new QPushButton(QStringLiteral("Just a button"), this));
+  group1->addWidget(new QPushButton(QStringLiteral("Just another button"), this));
+
+  connect(group1, &QSint::ActionGroup::editButtonClicked,
+          []() {
+            qDebug() << "edit button clicked";
+          });
+
+  connect(group1, &QSint::ActionGroup::headerTextChanged,
+          [](const QString& text) {
+            qDebug() << "header text changed, new text: " << text;
+          });
+
+  QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+  //  sizePolicy.setHorizontalStretch(0);
+  //  sizePolicy.setVerticalStretch(0);
+  //  sizePolicy.setHeightForWidth(action_panel->sizePolicy().hasHeightForWidth());
+  action_panel->setSizePolicy(sizePolicy);
+
+  action_panel->addStretch();
+
+  verticalLayout->addWidget(action_panel);
+
+  auto label = new QLabel(QStringLiteral("<img src=':images/filesave.png'>Hello!"));
+  verticalLayout->addWidget(label);
+
+#if 0
   auto verticalSpacer = new QSpacerItem(20, 40,
                                         QSizePolicy::Minimum,
                                         QSizePolicy::Expanding);
 
   verticalLayout->addItem(verticalSpacer);
+#endif
 }
 
 /*
---
+++ thirdparty/qsint/src/Core/actionpanel/taskheader_p.h
@@ -0,0 +1,79 @@
+#ifndef TASKHEADER_P_H
+#define TASKHEADER_P_H
+
+#include "actionpanelscheme.h"
+#include "actionlabel.h"
+
+#include <QLabel>
+
+class QBoxLayout;
+class QLineEdit;
+
+namespace QSint
+{
+
+
+class TaskHeader : public QFrame
+{
+  Q_OBJECT
+
+  typedef QFrame BaseClass;
+
+  friend class ActionGroup;
+
+public:
+  TaskHeader(const QIcon &icon, const QString &title, bool expandable, QWidget *parent = 0);
+
+  inline bool expandable() const { return myExpandable; }
+  void setExpandable(bool expandable);
+
+  void setHasEditButton(bool has_edit_button);
+  void setTitleEditable(bool title_editable);
+
+  inline QBoxLayout* headerLayout()
+  {
+    return (QBoxLayout*)layout();
+  }
+
+  void setScheme(ActionPanelScheme *scheme);
+
+signals:
+  void activated();
+  void editButtonClicked();
+  void headerTextChanged(const QString& text);
+
+public slots:
+  void fold();
+  void titleEditFinished();
+
+protected slots:
+  void animate();
+
+protected:
+  virtual void paintEvent ( QPaintEvent * event );
+  virtual void enterEvent ( QEvent * event );
+  virtual void leaveEvent ( QEvent * event );
+  virtual void mouseReleaseEvent ( QMouseEvent * event );
+  virtual void keyPressEvent ( QKeyEvent * event );
+  virtual void keyReleaseEvent ( QKeyEvent * event );
+
+  bool eventFilter(QObject *obj, QEvent *event);
+
+  void changeIcons();
+
+  ActionPanelScheme *myScheme;
+
+  bool myExpandable;
+  bool m_over, m_buttonOver, m_fold;
+  double m_opacity;
+
+  ActionLabel *myTitle;
+  QLineEdit *myTitleEditor { nullptr };
+  ActionLabel *editButton { nullptr };
+  QLabel *myButton;
+};
+
+
+}
+
+#endif // TASKHEADER_P_H
