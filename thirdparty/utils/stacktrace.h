84fdfc91f716be7bc4adb4ca12cca3c8ff4cc0b5
Lutts Cao <lutts.cao@gmail.com>
2015-12-05 11:36:48 +0800

    more readable stacktrace

--- thirdparty/utils/stacktrace.h
+++ thirdparty/utils/stacktrace.h
@@ -8,6 +8,51 @@
 #include <stdlib.h>
 #include <execinfo.h>
 #include <cxxabi.h>
+#include <dirent.h>
+
+static inline int getFileAndLine (char* exec_path, char* addr,
+                                  char *file_path, size_t flen, int* line) {
+  static char buf[512];
+
+  // prepare command to be executed
+  // our program need to be passed after the -e parameter
+  sprintf (buf, "addr2line -C -e %s -f -i %s", exec_path, addr);
+  FILE* f = popen (buf, "r");
+
+  if (f == NULL) {
+    perror (buf);
+    return 0;
+  }
+
+  // get function name
+  fgets (buf, 512, f);
+
+  // get file and line
+  fgets (buf, 512, f);
+
+  if (buf[0] != '?') {
+    char *p = buf;
+
+    // file name is until ':'
+    while (*p != ':') {
+      p++;
+    }
+
+    *p++ = 0;
+    // after file name follows line number
+    strncpy(file_path, buf, flen);
+    sscanf (p,"%d", line);
+
+    return 1;
+  } else {
+    strncpy (file_path, "unkown", sizeof("unkown"));
+    *line = 0;
+
+    return 0;
+  }
+
+  pclose(f);
+}
 
 /** Print a demangled stack backtrace of the caller function to FILE* out. */
 static inline void print_stacktrace(FILE *out = stderr,
@@ -30,13 +75,17 @@ static inline void print_stacktrace(FILE *out = stderr,
   char **symbollist = backtrace_symbols(addrlist, addrlen);
 
   // allocate string which will be filled with the demangled function name
-  size_t funcnamesize = 256;
+  size_t funcnamesize = 512;
   char *funcname = (char *)malloc(funcnamesize);
 
   // iterate over the returned symbol lines. skip the first, it is the
   // address of this function.
   for (int i = 1; i < addrlen; i++) {
     char *begin_name = 0, *begin_offset = 0, *end_offset = 0;
+    char *begin_addr = nullptr;
+    char *end_addr = nullptr;
+
+    int seqno = i - 1;
 
     // find parentheses and +address offset surrounding the mangled name:
     // ./module(function+0x15c) [0x8048a6d]
@@ -45,14 +94,20 @@ static inline void print_stacktrace(FILE *out = stderr,
         begin_name = p;
       else if (*p == '+')
         begin_offset = p;
-      else if (*p == ')' && begin_offset) {
+      else if (*p == ')' && begin_offset)
         end_offset = p;
+      else if (*p == '[')
+        begin_addr = p;
+      else if (*p == ']' && begin_addr) {
+        end_addr = p;
         break;
       }
     }
 
-    if (begin_name && begin_offset && end_offset && begin_name < begin_offset) {
+    if (begin_name)
       *begin_name++ = '\0';
+
+    if (begin_name && begin_offset && end_offset && begin_name < begin_offset) {
       *begin_offset++ = '\0';
       *end_offset = '\0';
 
@@ -65,16 +120,30 @@ static inline void print_stacktrace(FILE *out = stderr,
           abi::__cxa_demangle(begin_name, funcname, &funcnamesize, &status);
       if (status == 0) {
         funcname = ret;  // use possibly realloc()-ed string
-        fprintf(out, "  %s : %s+%s\n", symbollist[i], funcname, begin_offset);
+        fprintf(out, "  #%d : %s+%s\n", seqno, funcname, begin_offset);
       } else {
         // demangling failed. Output function name as a C function with
         // no arguments.
-        fprintf(out, "  %s : %s()+%s\n", symbollist[i], begin_name,
+        fprintf(out, "  #%d : %s()+%s\n", seqno, begin_name,
                 begin_offset);
       }
     } else {
       // couldn't parse the line? print the whole line.
-      fprintf(out, "  %s\n", symbollist[i]);
+      fprintf(out, "  #%d : %s\n", seqno, symbollist[i]);
+    }
+
+    if (begin_addr) {
+      *begin_addr++ = '\0';
+      *end_addr = '\0';
+
+      char* file_path = funcname;
+      int lineno;
+      int found = getFileAndLine(symbollist[i], begin_addr,
+                                 file_path, funcnamesize, &lineno);
+      if (found) {
+        // emacs friendly
+        fprintf(out, "%s:%d: %s\n", file_path, lineno, begin_addr);
+      }
     }
   }
 
